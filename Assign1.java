805b2e30f86d901485efa536c4ac54230155895b
TEALE-ASUS\teale <tealemasrani@gmail.com>
2018-03-12 22:57:59 -0600

    Latest version- March 12. EnumDec could work better, need to get pathname and type as user input, need to better organize code

--- Assign1.java
+++ Assign1.java
@@ -1,6 +1,5 @@
+// Version: March 12
 
-import java.util.HashSet;
-import java.util.Set;
 import org.eclipse.jdt.core.dom.AST;
 import org.eclipse.jdt.core.dom.ASTNode;
 import org.eclipse.jdt.core.dom.ASTParser;
@@ -8,14 +7,11 @@ import org.eclipse.jdt.core.dom.ASTVisitor;
 import org.eclipse.jdt.core.dom.AnnotationTypeDeclaration;
 import org.eclipse.jdt.core.dom.CompilationUnit;
 import org.eclipse.jdt.core.dom.EnumDeclaration;
-import org.eclipse.jdt.core.dom.ITypeBinding;
-import org.eclipse.jdt.core.dom.MethodDeclaration;
-import org.eclipse.jdt.core.dom.SimpleName;
-import org.eclipse.jdt.core.dom.StringLiteral;
+import org.eclipse.jdt.core.dom.MarkerAnnotation;
+import org.eclipse.jdt.core.dom.NormalAnnotation;
+import org.eclipse.jdt.core.dom.SimpleType;
 import org.eclipse.jdt.core.dom.TypeDeclaration;
-import org.eclipse.jdt.core.dom.VariableDeclarationFragment;
-import org.eclipse.jdt.core.dom.WhileStatement;
-
+import org.eclipse.jdt.core.dom.PrimitiveType;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -23,44 +19,83 @@ import java.io.IOException;
 import java.util.Scanner;
 import java.io.File;
 
+/*
+ * Java type:
+ * Class declaration......DONE
+ * Interface declaration.....DONE
+ * Enum declaration....
+ * Annotation declaration......DONE
+ */
 
 public class Assign1 {
-	
-	
+
 	static int decCount = 0;
 	static int refCount = 0;
 	
-	//Ask the user for the pathname (command line)
-	public static String getPathname() {							
-		Scanner kb = new Scanner(System.in);						// Instantiate a scanner
-		System.out.print("Please enter the directory pathname: ");	// Ask user
-		String pathname = kb.next();								// Take in what they entered
-		kb.next();
-		kb.close();													// Close scanner
-		return pathname;											// Return the pathname 
+	public static void main (String [] args) throws FileNotFoundException, IOException {
+
+//		String pathname = getPathname();																// Ask user for directory path
+//		String pathname = "C:\\Users\\teale\\Documents\\School\\SENG 300\\SENGTutorial1\\src";			// DEBUGGING: Just put the pathname here
+//		String targetType = getType();																// Ask user for type they're looking for
+
+//		String pathname = "C:\\Users\\teale\\Documents\\School\\SENG 300\\SENGTutorial1\\src";			// Manual for now
+		String pathname = "/Users/ahmed/Downloads/test";
+		String targetType = "enum";																// DEBUGGING: Just put target type here
+//		String [] sources = {"C:\\Users\\teale\\Documents\\eclipse-java-oxygen-2-win32-x86_64\\eclipse\\plugins"};			// Jar files
+		String [] sources = {"/Users/ahmed/Downloads/jar"};
 		
+		//String pathname = getPathname();
+		//String targetType = getTargetType();
+		String sourceString = filesToString(pathname);												// Make a string of the files' code
+		String [] classpath = {pathname};															// Where the files are located
+		String unitName = sourceString;
+
+		ASTNode cu = makeSyntaxTree(sourceString.toCharArray(),classpath, sources, unitName); 			// Build syntax tree from the string file content
+
+		count(cu, targetType);																		// Traverse, count declarations of given type
+
+		System.out.printf("Type: %2s\tDeclarations found: %1d\tReferences found: %d \n",targetType,decCount,refCount);
+
 	}
+
+//	//Ask the user for the pathname (command line)
+//	public static String getPathname() {
+//		System.out.print("Please enter the directory pathname: ");	// Ask user
+//		Scanner keyboard = new Scanner(System.in);						// Instantiate a scanner
+//		String pathname = keyboard.nextLine();								// Take in what they entered
+//		//kb.next();
+//		keyboard.close();													// Close scanner
+//		return pathname;											// Return the pathname
+//	}
 	
+	public static String getTargetType() {
+		System.out.print("Please enter a fully qualified name of a java type: ");
+		Scanner keyboard = new Scanner (System.in);
+		String targetType = keyboard.nextLine();
+		keyboard.close();
+		return targetType;
+	}
+
 	// Convert all the content of the java files into one string to parse
 	public static String filesToString(String pathname) throws IOException, FileNotFoundException {
 		StringBuilder sb = new StringBuilder();							// Instantiate a String Builder
-		File directory = new File(pathname);							
+		File directory = new File(pathname);
 		File[] allFiles = directory.listFiles();						// Put files from directory into a list
-		
+
 		for (File f : allFiles) {										// For each file in the given directory
 			String fileName = f.getName().toLowerCase();				// Get the name of the file
-			
+
 			if (f.isFile() && fileName.endsWith(".java")) {				// If it's a java file
 				BufferedReader reader = null;							// Read each line
-				
+
 				try {
 					reader = new BufferedReader(new FileReader(f));		// Read the file
 					String aLine;
-					
+
 					while ((aLine = reader.readLine()) != null) {		// While the line isn't empty
 						sb.append(aLine);								// Append it to the string builder
 						sb.append(System.lineSeparator());				// Append a line break
-					}	
+					}
 				}
 				finally {
 					reader.close();										// Close the reader, on to next for-loop iteration...
@@ -69,136 +104,116 @@ public class Assign1 {
 		}
 		return sb.toString();											// Return the built string of code
 	}
-	
-	// Parse the syntax tree from the string of code
-	public static ASTNode makeSyntaxTree(char[] sourceCode) {
 
-		ASTParser parser = ASTParser.newParser(AST.JLS9);						
+	// Parse and traverse the syntax tree from the string of code
+	public static ASTNode makeSyntaxTree(char[] sourceCode,String[] classpath, String[] sources, String unitName ) {
+
+		ASTParser parser = ASTParser.newParser(AST.JLS9);
 		parser.setSource (sourceCode);
 		parser.setKind(ASTParser.K_COMPILATION_UNIT);
 		parser.setResolveBindings(true);
+		parser.setBindingsRecovery(true);
+		parser.setUnitName(unitName);																// Name of the file
+		parser.setEnvironment(classpath, sources, new String[] {"UTF-8"}, true);
+
 		final CompilationUnit cu = (CompilationUnit) parser.createAST(null);
-		
+
 		return cu;
 	}
-	
-	// Count class, enumeration, annotation, and interface type declarations
-	// and increment the decCount global variable
-	public static void countDeclarations(ASTNode cu, String targetType) {
+
+
+	public static void count(ASTNode cu, String targetType) {
 		cu.accept(new ASTVisitor() {
 			
-			public boolean visit(TypeDeclaration node) {						// Visit any Type Declaration nodes in the directory: (this kind of node
-				String nodeAsString = node.toString();							// includes Class Declarations and Interface Declarations.)
-				System.out.println("THIS IS ONE NODE \n" +nodeAsString); 		//DEBUGGING	
-				if (nodeAsString.contains("class "+ targetType) | nodeAsString.contains("interface "+ targetType) ) { // if this class or interface declaration includes the target type
-					decCount++;													// Add to count
-				}
+			// COUNT ANNOTATION DECLARATIONS
+			public boolean visit(AnnotationTypeDeclaration node) {									// Visit any Annotation Declaration nodes in the directory
+				System.out.println("<< Annotation Type ??>>");
+				String nodeAsString = node.toString();
+				//				System.out.println("THIS IS ONE NODE \n" +nodeAsString); 								// DEBUGGING
+				//System.out.println("<<HAKUNA MATATA>>");
+				if ( nodeAsString.contains("interface "+ targetType) )							// if this annotation declaration includes the target type
+					decCount++;																		// Add to count
+				
 				return true;
 			}
 			
-			public boolean visit(EnumDeclaration node) {						// Visit any Enumeration Declaration nodes in the directory
-				String nodeAsString = node.toString();							
-				System.out.println("THIS IS ONE NODE \n" +nodeAsString); 		// DEBUGGING	
-				if (nodeAsString.contains("enum "+ targetType)) { 				// if this enumeration declaration includes the target type
-					decCount++;													// Add to count
+			// COUNT ENUMERATION DECLARATIONS
+			public boolean visit(EnumDeclaration node) {												// Visit any Enumeration Declaration nodes in the directory
+				String nodeAsString = node.toString();
+				System.out.println("THIS IS ONE NODE \n" +nodeAsString); 								// DEBUGGING
+				System.out.println("<< check enum?? >>");
+				if (nodeAsString.contains("enum "+ targetType)) { 									// if this enumeration declaration includes the target type
+					decCount++;																		// Add to count
 				}
+				return true;
+			}
+
+			// COUNT CLASS AND INTERFACE DECLARATIONS
+			public boolean visit(TypeDeclaration node) {												// Visit any Type Declaration nodes in the directory: (this kind of node
+				//System.out.println("<< type = "+ node.resolveBinding().getQualifiedName()+" >>");
+				if ( node.resolveBinding().getQualifiedName().equals(targetType) ) {
+					decCount++;		
+					}																				// Add to count
+
+				return true;
+			}
+
+			public boolean visit (NormalAnnotation node) {
+			//	System.out.println("<< In annotation >>");
+				if ( node.resolveTypeBinding().getQualifiedName().equals(targetType) ) {
+					refCount++;
+				}
+
 				return true;
 			}
 			
-			public boolean visit(AnnotationTypeDeclaration node) {				// Visit any Annotation Declaration nodes in the directory
-				String nodeAsString = node.toString();							
-				System.out.println("THIS IS ONE NODE \n" +nodeAsString); 		// DEBUGGING	
-				if (nodeAsString.contains("interface "+ targetType)) { 			// if this annotation declaration includes the target type
-					decCount++;													// Add to count
+//		public boolean visit(VariableDeclarationFragment node) {									// Visit any Type Declaration nodes in the directory:
+////		System.out.println("THIS IS ONE NODE \n" +node.toString()); //DEBUGGING				// (this kind of node includes Class Declarations
+//		SimpleName name = node.getName();
+//		//IVariableBinding binding = node.resolveBinding();
+//		//String nodeType = binding.getQualifiedName();
+//		//String classType = binding.getDeclaringClass().getName();							// Trying to get class name
+//		//System.out.println("<< Annotations type = "+binding.+" >>");								// Print class type. Might give null pointer error
+//		//if ((nodeType).equals(targetType)){refCount++;}
+//		System.out.printf("<< Variable = %s\t\tType = %s >>\n",name,node.resolveBinding().getType().getQualifiedName());
+////		if ( nodeType.equals(targetType)) {
+////			System.out.println("<<Test in VaribleDecFrag: refCount ++ >>");
+//////			decCount++;
+////			refCount++;
+//		return true;
+//		}
+		
+			public boolean visit (MarkerAnnotation node) {
+			//	System.out.println("<< In annotation >>");
+				if ( node.resolveTypeBinding().getQualifiedName().equals(targetType) ) {
+				//	System.out.println("Count ++");
+					refCount++;
 				}
+
 				return true;
 			}
+
+			public boolean visit(SimpleType node) { 
+			//	System.out.println("<< In SimpleType >>" + node.resolveBinding().getQualifiedName());
+				if ( node.resolveBinding().getQualifiedName().equals(targetType) ) {
+				//	System.out.println("Count ++");
+					refCount++;
+				}
+
+				return true;
+			}
+
+
+			public boolean visit(PrimitiveType node) {	
+				//System.out.println("name = "+node.resolveBinding().getQualifiedName());
+				if ( node.resolveBinding().getQualifiedName().equals(targetType) & !(node.resolveBinding().getQualifiedName().equals("void"))) {
+					refCount++;
+				}
+
+				return true;
+			}
+
 		});
 	}
 
-	public static void main (String [] args) throws FileNotFoundException, IOException {
-		
-//		String pathname = getPathname();										// Ask user for directory path
-		String pathname = "C:\\Users\\teale\\Documents\\School\\SENG 300\\SENGTutorial1\\src";	// DEBUGGING: Just put the pathname here
-//		String targetType = getType();											// Ask user for type they're looking for
-		String targetType = ("Cuboid");											// DEBUGGING: Just put target type here	
-		String sourceString = filesToString(pathname);							// Make a string of the files' code
-		System.out.println("THIS IS THE ENTIRE SOURCE STRING"+sourceString);  	// FOR DEBUGGING
-		ASTNode cu = makeSyntaxTree(sourceString.toCharArray()); 				// Build syntax tree from the string file content
-		countDeclarations(cu, targetType);										// Traverse, count declarations of given type
-		System.out.println(decCount); 											// DEBUG print the declaration count for a given type 
-	}
-}  
-		
-				/** Our previous shenanigans **/		
-		
-//	}
-/*
-		ASTParser parser = ASTParser.newParser(AST.JLS3);
-
-		parser.setSource ("public class A {\\n String s = \"astring\";  \\n int j; \\n int i = 9; while(i>0){i--;}}".toCharArray());
-		
-		parser.setKind(ASTParser.K_COMPILATION_UNIT);
- 
-		parser.setResolveBindings(true);
-		
-		ASTNode cu = parser.createAST(null);	// cu contains one node
-		
-//		AST tree = cu.getAST();					// tree contains the tree from cu node 
-		
-//		ASTNode root = cu.getRoot();			// root contains root node from cu
-
-//		System.out.println(root.toString());
-		
-		cu.accept(new ASTVisitor() {
-			
-//			Set names = new HashSet();
- 
-			// This is from that website I sent Max
-			public boolean visit(WhileStatement node) {
-				System.out.println(node.toString());
-				int type = node.getNodeType(); 
-				System.out.println(type);
-				System.out.println(node.getParent().toString());
-				System.out.println(node.getParent().getNodeType());
-				
-//				SimpleName name = node.getName();
-//				this.names.add(name.getIdentifier());			// Dont know what this line was for
-//				System.out.println("Declaration of "+name);
-				return true; 
-				
-			
-//			public boolean visit (VariableDeclarationFragment node) {
-//				String stringofNode = root.toString();				//make string representation of node
-//				System.out.println(stringofNode);					//print the current node in a string
-//				System.out.println(root.getName());
-//				if (root.getNodeType() == 15) {
-//					System.out.println("hshdhs");
-//				return true;
-			}
-//		boolean test = visit();
-
-		});
-
-//		ASTNode a = cu.getRoot();
-
-//		System.out.println(a.getNodeType());
-
-//		visit();
-
-
-
-
-	}
-
-/*	public boolean visit(VariableDeclarationFragment node) {
-		SimpleName name = node.getName();
-		//this.names.add(name.getIdentifier());
-		System.out.println("Declaration of '"+name+"' at line");
-
-		return true;
-*/
-	
-	
-
-
+}
