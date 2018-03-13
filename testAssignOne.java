83e885779ca6ce47332b09abbc082e8a20e67610
Max Dou <max.dou6@gmail.com>
2018-03-12 20:11:41 -0600

    Add files via upload
    Test cases for the automated tests.

---
+++ testAssignOne.java
@@ -0,0 +1,80 @@
+import static org.junit.Assert.*;
+
+import java.util.Map;
+
+import org.eclipse.jdt.core.dom.AST;
+import org.eclipse.jdt.core.dom.ASTNode;
+import org.eclipse.jdt.core.dom.ASTParser;
+import org.eclipse.jdt.core.dom.ASTVisitor;
+import org.eclipse.jdt.core.dom.AbstractTypeDeclaration;
+import org.eclipse.jdt.core.dom.AnnotatableType;
+import org.eclipse.jdt.core.dom.Annotation;
+import org.eclipse.jdt.core.dom.AnnotationTypeDeclaration;
+import org.eclipse.jdt.core.dom.AnonymousClassDeclaration;
+import org.eclipse.jdt.core.dom.CompilationUnit;
+import org.eclipse.jdt.core.dom.EnumConstantDeclaration;
+import org.eclipse.jdt.core.dom.EnumDeclaration;
+import org.eclipse.jdt.core.dom.IMethodBinding;
+import org.eclipse.jdt.core.dom.ITypeBinding;
+import org.eclipse.jdt.core.dom.IVariableBinding;
+import org.eclipse.jdt.core.dom.LambdaExpression;
+import org.eclipse.jdt.core.dom.MarkerAnnotation;
+import org.eclipse.jdt.core.dom.MethodDeclaration;
+import org.eclipse.jdt.core.dom.NormalAnnotation;
+import org.eclipse.jdt.core.dom.SimpleName;
+import org.eclipse.jdt.core.dom.SimpleType;
+import org.eclipse.jdt.core.dom.SingleMemberAnnotation;
+import org.eclipse.jdt.core.dom.TypeDeclaration;
+import org.eclipse.jdt.core.dom.VariableDeclarationFragment;
+import org.eclipse.jdt.core.dom.PrimitiveType;
+
+import java.awt.List;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class testAssignOne {
+	
+	/**
+	 * test for method getPathname
+	 */
+	 @TestD
+	 pubic void testGetPathname(){
+		 //set
+		 Assign1 testCase = new Assign1()
+		 testCase.
+		 
+	 }
+	 
+	 /**
+	 * test for method filesToSTring
+	 */
+	 @TestD
+	 pubic void testFilesToString(){
+		 //set
+		 Assign1 testCase = new Assign1()
+		 
+	 }
+	 
+	 /**
+	 * test for method makeSyntaxTree
+	 */
+	 @TestD
+	 pubic void testMakeSyntaxTree(){
+		 //set
+		 Assign1 testCase = new Assign1()
+	 }
+	 
+	 /**
+	 * test for method count
+	 */
+	 @TestD
+	 pubic void testCount(){
+		//set
+		Assign1 testCase = new Assign1()
+		 
+	 }
+	
+}
