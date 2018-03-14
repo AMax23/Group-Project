8a2ecc579fc688893cda5375c0d34006191982c7
Max Dou <max.dou6@gmail.com>
2018-03-13 22:15:44 -0600

    Update UserContact.java

--- UserContact.java
+++ UserContact.java
@@ -5,25 +5,22 @@ import java.util.Scanner;
 public class UserContact {
 
 	//Ask the user for the pathname (command line)
-	public String getPathname() {
-		System.out.print("Please enter the directory pathname: ");
-		Scanner keyboard = new Scanner(System.in);			
+	public String getPathname(Scanner keyboard) {
+		System.out.print("Please enter the directory pathname: ");			
 		String pathname = keyboard.nextLine();							
 		return pathname;
 	}
 	
 	//Ask the user for the pathname (command line)
-	public String getTargetType() {
+	public String getTargetType(Scanner keyboard) {
 		System.out.print("Please enter a fully qualified name of a java type: ");
-		Scanner keyboard = new Scanner (System.in);
 		String targetType = keyboard.nextLine();
 		return targetType;
 	}
 
 	//Ask the user for the pathname for the jar files (command line)
-	public String[] getJarFiles() {
+	public String[] getJarFiles(Scanner keyboard) {
 		System.out.print("Please enter the pathname for the directory in which your JDT DOM jar files are kept: ");
-		Scanner keyboard = new Scanner (System.in);
 		String[] sources = {keyboard.nextLine()};
 		return sources;
 	}
